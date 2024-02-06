6ec7e7331f0910104dc4e901ef26327c88102002
Samuel Gomez <gocs020101@gs.utm.mx>
2024-02-05 18:42:55 -0600

    Solves bug!

--- src/parser/primeros_siguientes.py
+++ src/parser/primeros_siguientes.py
@@ -1,3 +1,22 @@
+from tkinter import filedialog
+
+def get_reglas(path):
+    with open(path, 'r') as file:
+        content = file.readlines()
+    content=' '.join(content)
+    content=content.split("\n")
+    content ='-|-'.join(content)
+    content=content.split("-> ")
+    content =''.join(content)
+    content=content.split("-|- ")
+    no_terminales = content[0].strip()
+    terminales = content[1].strip()
+    content.pop(0)
+    content.pop(0)
+    content.pop(0)
+
+    return no_terminales, terminales, content
+
 def obtener_Primeros(S,regla,noTerminales,terminales,bandera=0):
     v=0
     a=0
@@ -11,13 +30,7 @@ def obtener_Primeros(S,regla,noTerminales,terminales,bandera=0):
     for i in regla:
         i=i.split(" ")
         b=1
-        
-        
         i = [conta for conta in i if conta != '']
-        
-        
-        
-        
         if i[0]==S :
                 if len(i)>2:
                     bandera=1
@@ -41,6 +54,7 @@ def obtener_Primeros(S,regla,noTerminales,terminales,bandera=0):
     if a==0 and bandera==1:
         bandera=0
     return bandera,primeros[0]  
+
 def obtener_siguientes(i,regla,noterminales,terminale,primeros,sig):
     siguiente=""
     for reg in regla:
@@ -125,81 +139,81 @@ def obtener_siguientes(i,regla,noterminales,terminale,primeros,sig):
 #,"D @"]
 
 def primeros_siguientes(noterminales,terminale,regla):
-    noterminales="P T F L D I V N E K R B O H S W A X Y M Z G J Q"
+    # noterminales="P T F L D I V N E K R B O H S W A X Y M Z G J Q"
     noterminales=noterminales.split(" ")
-    terminale="( ) { } main id , ; [ ] int float printf literalCad scanf & nint nfloat * + - / % = if else while for switch case : < > ! | do return"
+    # terminale="( ) { } main id , ; [ ] int float printf literalCad scanf & nint nfloat * + - / % = if else while for switch case : < > ! | do return"
     terminale=terminale.split(" ")
 
-    regla=[
-    "P T F ( L ) { D I O H X A W G }",
-    "T int ",
-    "T float",
-    "F main",
-    "F id",
-    "L T P id V L",
-    "L , P id V L",
-    "L @",
-    "D T P id V L ; D",
-    "D @",
-    "I printf ( literalCad ) ; I",
-    "I scanf ( literalCad , & id ) ; I",
-    "I @",
-    "V [ N ]",
-    "V @",
-    "N nint ",
-    "N @",
-    "E nfloat",
-    "E @",
-    "P * ",
-    "P +",
-    "P -",
-    "P /",
-    "P %",
-    "P @",
-    "K { R }",
-    "K R",
-    "K B P B",
-    "K B + +",
-    "K B - -",
-    "R N E R",
-    "R , N E R",
-    "R @",
-    "B id",
-    "B nint",
-    "B nfloat",
-    "B @",
-    "O B = O ; O",
-    "O B P B P",
-    "O B",
-    "O @",
-    "H if ( B Z B ) { I } S",
-    "H I",
-    "S else { H }",
-    "S @",
-    "W while ( B Z B ) { J O M }",
-    "A for ( B Z B ; B Z B ; K ) { O A }",
-    "A @",
-    "X switch ( id ) { Y } ",
-    "Y case int :  O ; break ; Y",
-    "Y @",
-    "M M - = O ;",
-    "M M + = O ;",
-    "M M + + ;",
-    "M M - - ;",
-    "M id",
-    "M nint ",
-    "Z Z =",
-    "Z =",
-    "Z <",
-    "Z !",
-    "Z >",
-    "Z Z |",
-    "Z |",
-    "Z Z &",
-    "Z &",
-    "G T F ( L ) { Q }",
-    "J do { I } while ( B Z B Z B Z B ) ;",
-    "Q return O ;",]
+    #regla=[
+    #"P T F ( L ) { D I O H X A W G }",
+    #"T int ",
+    #"T float",
+    #"F main",
+    #"F id",
+    #"L T P id V L",
+    #"L , P id V L",
+    #"L @",
+    #"D T P id V L ; D",
+    #"D @",
+    #"I printf ( literalCad ) ; I",
+    #"I scanf ( literalCad , & id ) ; I",
+    #"I @",
+    #"V [ N ]",
+    #"V @",
+    #"N nint ",
+    #"N @",
+    #"E nfloat",
+    #"E @",
+    #"P * ",
+    #"P +",
+    #"P -",
+    #"P /",
+    #"P %",
+    #"P @",
+    #"K { R }",
+    #"K R",
+    #"K B P B",
+    #"K B + +",
+    #"K B - -",
+    #"R N E R",
+    #"R , N E R",
+    #"R @",
+    #"B id",
+    #"B nint",
+    #"B nfloat",
+    #"B @",
+    #"O B = O ; O",
+    #"O B P B P",
+    #"O B",
+    #"O @",
+    #"H if ( B Z B ) { I } S",
+    #"H I",
+    #"S else { H }",
+    #"S @",
+    #"W while ( B Z B ) { J O M }",
+    #"A for ( B Z B ; B Z B ; K ) { O A }",
+    #"A @",
+    # "X switch ( id ) { Y } ",
+    # "Y case int :  O ; break ; Y",
+    # "Y @",
+    # "M M - = O ;",
+    # "M M + = O ;",
+    # "M M + + ;",
+    # "M M - - ;",
+    # "M id",
+    # "M nint ",
+    # "Z Z =",
+    # "Z =",
+    # "Z <",
+    # "Z !",
+    # "Z >",
+    # "Z Z |",
+    # "Z |",
+    # "Z Z &",
+    # "Z &",
+    # "G T F ( L ) { Q }",
+    # "J do { I } while ( B Z B Z B Z B ) ;",
+    # "Q return O ;",]
     primeros={}
     
     for i in noterminales:
@@ -227,4 +241,6 @@ def primeros_siguientes(noterminales,terminale,regla):
         siguiente[i]=' '.join(siguiente[i])
     print(siguiente)
 
-primeros_siguientes("","","")
+path = filedialog.askopenfilename(filetype=[("Archivos de texto", "*.txt")])
+no_terminales, terminales, reglas = get_reglas(path)
+primeros_siguientes(no_terminales, terminales, reglas)
